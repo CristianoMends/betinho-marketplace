38dcc39c6764eb8e21755cd2ec017be177e2fd13
CristianoMends <mendescristiano012@gmail.com>
2024-01-20 12:04:42 -0300

    atualizacao interface e funcoes

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Simulação de dados do catálogo
     const products = [
-        { id: 'chumbinho', image: '../img/chumbinho.jpeg', name: 'Chumbinho Carabina de pressão 5.5', description: 'Chumbinho 5.5mm Espingarda De Pressão Carabina', price: 'R$ 19,99' },
-        { id: 'carburador', image: '../img/carburador.jpeg', name: 'Carburador para motoserra', description: 'Carburador para motoserra Ms 170 Ms 180', price: 'R$ 29,99' },
-        { id: 'esticador', image: '../img/esticador.jpeg', name: 'Esticador para corrente', description: 'Esticador para corrente de motoserra', price: 'R$ 39,99' }
+        { id: 'chumbinho', image: 'img/chumbinho.jpeg', name: 'Chumbinho para Carabina de pressão', description: 'Chumbinho 5.5mm Espingarda De Pressão Carabina', price: '15,00' },
+        { id: 'carburador', image: 'img/carburador.jpeg', name: 'Carburador para motoserra', description: 'Carburador para motoserra Ms 170 Ms 180', price: '150,00' },
+        { id: 'esticador', image: 'img/esticador.jpeg', name: 'Esticador para corrente', description: 'Esticador para corrente de motoserra', price: '25,00' }
     ];
 
     const productSection = document.getElementById('productSection');
@@ -26,14 +26,26 @@ document.addEventListener('DOMContentLoaded', function () {
         description.textContent = product.description;
 
         const price = document.createElement('p');
-        price.textContent = `Preço: ${product.price}`;
+        price.className = 'price';
+        price.textContent = `R$: ${product.price}`;
 
         const button = document.createElement('button');
         button.textContent = 'Consultar o Vendedor';
+        button.addEventListener('click',function(){
+            var numeroTelefone = "558894748694";
+
+            var mensagemPredefinida = "Olá, gostaria de mais informações sobre o produto: " + product.name;
+
+            var mensagemCodificada = encodeURIComponent(mensagemPredefinida);
+
+            var linkWhatsAppWeb = "https://web.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + mensagemCodificada;
+
+            window.open(linkWhatsAppWeb, '_blank');
+        })
 
         productInfo.appendChild(h2);
-        productInfo.appendChild(description);
         productInfo.appendChild(price);
+        productInfo.appendChild(description);
         productInfo.appendChild(button);
 
         article.appendChild(img);
@@ -41,4 +53,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
         productSection.appendChild(article);
     });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const galeria = document.querySelector('.galeria');
+    let contadorImagem = 1;
+
+    setInterval(() => {
+        galeria.style.transform = `translateX(${-contadorImagem * 100}%)`;
+        contadorImagem++;
+
+        if (contadorImagem === document.querySelectorAll('.galeria img').length) {
+            contadorImagem = 0;
+        }
+    }, 3000);
 });
